e7d3485966b4827dcc3d11b0be7a905521db40ab
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2018-08-08 04:23:52 +0000

    hide index

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 /*--- Write Javascript Here ---*/
+// Get UI Elements
+const img = document.querySelector("img");
+const body = document.querySelector("body");
+const p = document.querySelector("p");
+const container = document.querySelector(".container");
+ 
+// Add Event Listener
+container.addEventListener("mousemove", moveImg);
+ 
+// Move Image Function
+function moveImg(e) {
+  console.log(e.target);
+  p.innerHTML = `Position X: ${e.clientX} <br/> Position Y: ${e.clientY}`;
+  img.style.left = `${e.clientX}px`;
+  img.style.top = `${e.clientY}px`;
+}
